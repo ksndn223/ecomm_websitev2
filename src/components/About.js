2efef24dfc5bfa07a1ca357c59c037378cde93e4
ksndn223 <11366993+ksndn223@users.noreply.github.com>
2024-01-07 04:29:30 -0500

    Created About.js

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+const About = () => {
+    return ( 
+         
+        <div className='container-fluid'>
+         <div class="container p-1 mx-0">
+        <h2 className="mt-5 mb-2"> About DELL Technologies </h2>
+        </div>
+        <h2 className="mt-10 mb-2"> Who We Are</h2>
+        <p> We are a diverse team with unique perspectives. United in our purpose, our strategy and our culture. Driven by our ambition and 
+            the power of technology to drive human progress. Unwavering in our commitment to equality, trust and advocacy for one another.
+
+        </p>
+
+        <h2 className="mt-10 mb-2"> We create technologies that drive human progress</h2>
+        <p> Our story began with a belief and a passion: that everybody should have easy access to the best technology anywhere in the world. 
+            That was in 1984 in Michael Dell’s University of Texas dorm room. Today, Dell Technologies is instrumental in changing the digital 
+            landscape the world over. </p>
+        
+        </div>
+       
+        
+        
+        
+        
+      
+   
+     );
+}
+ 
+export default About;
