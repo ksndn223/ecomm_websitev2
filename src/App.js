a519a86aba39469621ea390e05707a65968c2f2a
ksndn223 <11366993+ksndn223@users.noreply.github.com>
2024-01-07 04:27:09 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Products from './components/Products';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import Footer from './components/Footer';
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Footer/>
+   
+      <Routes> 
+      <Route path="/home" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/products" element={<Products/>}></Route>
+        <Route path="/gallery" element={<Gallery/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+
+      </Routes>
+
     </div>
   );
 }
