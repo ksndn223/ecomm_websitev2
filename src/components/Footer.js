9fa74ec64b33f88a4ff8c1086710a8aa7bda254f
ksndn223 <11366993+ksndn223@users.noreply.github.com>
2024-01-07 04:42:09 -0500

    Created Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer  = () => {
+    return ( 
+        <div className="">
+        <footer class="bg-primary text-black text-center py-3 mt-4 fixed-bottom">
+        &copy; DELL Technologies
+       </footer>
+        </div>
+     );
+}
+ 
+export default Footer;
